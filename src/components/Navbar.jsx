de6a6f8bc0f4eee57d94f77489884031b1b3ed2e
alhameen jimoh <alhameenjimoh4@gmail.com>
2025-11-13 15:06:00 +0100

    adding albums and activating buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
+import React, { use } from 'react'
 import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+
+  const navigate = useNavigate()
+
+
   return (
     <>
       <div className="w-full flex justify-between items-center font-semibold">
         <div className="flex items-center gap-2">
           <img
+            onClick={() => navigate(-1)}
             className="w-8 bg-black p-2 rounded-2xl cursor-pointer"
             src={assets.arrow_left}
             alt=""
           />
           <img
+            onClick={() => navigate(+1)}
             className="w-8 bg-black p-2 rounded-2xl cursor-pointer"
             src={assets.arrow_right}
             alt=""
@@ -29,10 +36,14 @@ const Navbar = () => {
           </p>
         </div>
       </div>
-      <div className='flex items-center gap-2 mt-4'>
-        <p className='bg-white text-black px-4 py-1 rounded-2xl cursor-pointer'>All</p>
-        <p className='bg-black px-4 py-1 rounded-2xl cursor-pointer'>Music</p>
-        <p className='bg-black px-4 py-1 rounded-2xl cursor-pointer'>Podcasts</p>
+      <div className="flex items-center gap-2 mt-4">
+        <p className="bg-white text-black px-4 py-1 rounded-2xl cursor-pointer">
+          All
+        </p>
+        <p className="bg-black px-4 py-1 rounded-2xl cursor-pointer">Music</p>
+        <p className="bg-black px-4 py-1 rounded-2xl cursor-pointer">
+          Podcasts
+        </p>
       </div>
     </>
   );
